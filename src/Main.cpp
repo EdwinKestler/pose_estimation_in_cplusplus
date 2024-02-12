afc335bf4d5aba69167f25a9327f558a3af026e7
Edwin Kestler <kestler@flatbox.co>
2024-02-12 16:19:12 -0600

    update

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
 
 
 using namespace cv;
 
 int main()
 {
-    const std::string RTSP_URL = "rtsp://user:pass@192.168.0.189:554/h264Preview_01_main";
-
-#if WIN32
-    _putenv_s("OPENCV_FFMPEG_CAPTURE_OPTIONS", "rtsp_transport;udp");
-#else
-    setenv("OPENCV_FFMPEG_CAPTURE_OPTIONS", "rtsp_transport;udp", 1);
-#endif
+    const std::string rstp_url = "rtsp://admin:FBX!admin2023@192.168.1.108:554";
 
     Mat frame;
-    VideoCapture cap(RTSP_URL, CAP_FFMPEG);
+    VideoCapture cap(rstp_url, cv::CAP_FFMPEG);
 
     if (!cap.isOpened()) {
         std::cout << "Cannot open RTSP stream" << std::endl;
